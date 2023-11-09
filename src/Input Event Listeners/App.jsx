cdae369bb3b3f7b4cd2ab09984515440213c9469
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-11-09 09:51:23 -0500

    event listeners lesson

--- src/Input Event Listeners/App.jsx
+++ src/Input Event Listeners/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
+
 function App() {
-	return <div>hi</div>
+	const [name, setName] = useState("Sean")
+
+	return (
+		// using value and onChange is a "controled" input meaning React has control on the input.
+		// using defaultValue is an "uncontrolled" input meaning the the browser has control on the input.
+		<div>
+			<label htmlFor="">Controlled input:</label>
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<br />
+			<br />
+			<label htmlFor="">Uncontrolled input:</label>
+			<input type="text" defaultValue={name} />
+		</div>
+	)
 }
 
 export default App
